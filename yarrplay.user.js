00c69273d5714e2da08dd6f84bdd5ce44a49a4ad
aria <87399016+tacohitbox@users.noreply.github.com>
2022-01-17 17:15:20 +0000

    v1.1

--- yarrplay.user.js
+++ yarrplay.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YarrPlay
-// @version      1.0
+// @version      1.1
 // @description  Play/download movies right on the IMDb page of each show/movie.
 // @author       tacohitbox
 // @match        https://www.imdb.com/title/tt*
@@ -8,9 +8,11 @@
 // @grant        none
 // ==/UserScript==
 
-if (getType() == "s") {return;}
 console.log(getType());
-document.querySelectorAll(".dDhYrh *").forEach(function(el) {el.remove();})
+if (getType() !== "s") {
+  document.querySelectorAll(".dDhYrh *").forEach(function(el) {el.remove();})
+}
+
 
 var i = document.createElement("iframe");
 if (getType() == "movie") {
@@ -18,17 +20,40 @@ if (getType() == "movie") {
   i.src = `https://vidsrc.me/embed/${idm}`;
   i.style = "width:100%;height:100%;display:block;border:0;";
   i.setAttribute("allowfullscreen", "");
-  document.querySelector(".dDhYrh").append(i);
+  if (document.querySelector(".dDhYrh")) {
+    document.querySelector(".dDhYrh").append(i);
+  } else {
+    i.style = "width:100%;height:325px;display:block;border:0;margin-top:10px;";
+    document.querySelector(".dYxvce").append(i);
+  }
 } else if (getType() == "tv") {
   var idp = `tt${document.querySelector(".ipc-link").href.split("/tt")[1].split("/")[0]}`;
-  var s = document.querySelectorAll(".eHqAZ")[0].innerHTML.split("S")[1];
-  var e = document.querySelectorAll(".eHqAZ")[1].innerHTML.split("E")[1];
-  i.src = `https://vidsrc.me/embed/${idp}/${s}-${e}`;
-  i.style = "width:100%;height:325px;display:block;border:0;margin-top:10px;";
-  i.setAttribute("allowfullscreen", "");
-  document.querySelector(".iuGjVe").append(i);
+  if (document.querySelectorAll(".eHqAZ").length !== 0) {
+    var s = document.querySelectorAll(".eHqAZ")[0].innerHTML.split("S")[1];
+    var e = document.querySelectorAll(".eHqAZ")[1].innerHTML.split("E")[1];
+  } else if (document.querySelectorAll(".gwHgBk").length !== 0) {
+    var s = document.querySelectorAll(".gwHgBk")[0].innerHTML.split("S")[1];
+    var e = document.querySelectorAll(".gwHgBk")[1].innerHTML.split("E")[1];
+  }
+  if (typeof s == "string") {
+    i.src = `https://vidsrc.me/embed/${idp}/${s}-${e}`;
+    i.setAttribute("allowfullscreen", "");
+    if (document.querySelector(".dDhYrh")) {
+      i.style = "width:100%;height:100%;display:block;border:0;";
+      document.querySelector(".dDhYrh").append(i);
+    } else if (document.querySelector(".iEHpKn")) {
+      i.style = "width:100%;height:325px;display:block;border:0;margin-top:10px;";
+      document.querySelector(".iEHpKn").append(i);
+    }
+  }
 }
 
 function getType() {
-  if (document.querySelector(".episode-guide-text") == null && document.querySelector(".klVacg") == null) {return "movie";} else if (document.querySelector(".gCtawA") == null) {return "s";} else {return "tv";}
+  if (
+    document.querySelector(".jswntl") == null &&
+    document.querySelector(".episode-guide-text") == null &&
+    document.querySelector(".dYxvce") !== null
+  ) {return "movie";}
+  else if (document.querySelector(".gCtawA") == null && document.querySelector(".dDhYrh") == null && document.querySelector(".ebbHRh") == null || document.querySelector(".kzEMhq") !== null) {return "s";}
+  else {return "tv";}
 }
